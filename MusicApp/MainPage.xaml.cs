9d13b0ea99b7a274ac122f3ba188748a031eddbb
Student <Student@10ffe0af96a1>
2025-10-28 10:21:18 +0100

    Dodaj pliki projektów.

---
+++ MusicApp/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ MusicApp/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MusicApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Poppins-Black.ttf", "Poppins-Black");
+                    fonts.AddFont("Poppins-Light.ttf", "Poppins-Light");
+
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
